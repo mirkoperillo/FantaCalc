f23d2183c230f23ee79fd4e6b273f9992f98af9a
mirkoperillo <mirko.perillo@gmail.com>
2017-06-30 17:25:23 +0200

    rules extends propertybonus class

--- src/main/java/app/rules/impl/GoalsRule.java
+++ src/main/java/app/rules/impl/GoalsRule.java
@@ -1,13 +1,14 @@
 package app.rules.impl;
 
 import app.model.Player;
+import app.rules.PropertyBonus;
 import app.rules.Rule;
 
-public class GoalsRule implements Rule {
+public class GoalsRule extends PropertyBonus implements Rule {
 
 	@Override
 	public double apply(Player player) {
-		return player.getMatchReport().getGoals() * 3;
+		return player.getMatchReport().getGoals() * getBonusValue("goal_segnato");
 	}
 
 }
